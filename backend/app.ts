e7e6733a1a0c6f39561e0f58fe9982989bdd01d4
André Luiz <alps2@cin.ufpe.br>
2021-12-12 16:58:50 -0300

    Changed backend API default port

--- backend/app.ts
+++ backend/app.ts
@@ -77,7 +77,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/public/index.html'))
   })
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 8001, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     //console.log(testProcessInput());
 });
